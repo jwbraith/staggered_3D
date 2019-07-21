48bb746610f254eceb78d22a73dce8b769260a5d
jwbraith <jwbraith@gmail.com>
2019-07-21 01:31:58 -0700

    3D experiment enh

--- sketch.js
+++ sketch.js
@@ -7,24 +7,33 @@ let rates = [];
 
 
 function setup() {
-	createCanvas(400, 400);
+	createCanvas(400, 400, WEBGL);
 	background(0);
+
 }
 
 function draw() {
 	background(0);
+	translate(-200,0,-1000);
+
+	rotateY(PI/6);
+	normalMaterial();
 	let offset = 0;
-	translate(width/2,height/2)
+	// translate(width/2,height/2)
 	for (let i = 0;i < 6;i++) {
 		let a = angle + offset;
 		let s = spacing+offset;
-		let oss = 20 * sin(s)
-		let x = 100 * sin(a);
-		let y = 100 * cos(a) + (i*oss);
-		ellipse(x,y,40);
-		offset += 0.23;
+		let oss = 100 * sin(s)
+		let x = 175 * sin(a);
+		let y = 175 * cos(a) + (i*oss);
+		let z = 175 * sin(a) + (i*150);
+		push();
+		translate(x,y,z);
+		sphere(40);
+		pop();
+		offset += 0.55;
 
 	}
-	angle += 0.05;
+	angle += 0.07;
 	spacing += 0.01;
 }
